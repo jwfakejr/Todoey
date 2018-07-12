f24fd195167b522bb48e831df6dcbb99717c0759
John Fake <jwfakejr@gmail.com>
2018-07-12 10:05:34 -0500

    Convert to MVC design pattern

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by John Fake on 7/12/18.
+//  Copyright © 2018 John Fake. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
